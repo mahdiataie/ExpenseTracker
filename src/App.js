a6807cf11b258faae7642de6c82bbb175f8f21cf
Mahdi Ataei <54065538+mahdiataie@users.noreply.github.com>
2022-09-30 13:22:30 +0200

    First

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses";
+function App() {
+  
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses/>
+    </div>
+  );
+}
+
+export default App;
